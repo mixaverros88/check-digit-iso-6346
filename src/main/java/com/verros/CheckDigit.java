051007848fdaa756c7a94294b8f5c8a097246b3b
mixaverros88 <mixalisverros@hotmail.gr>
2020-03-12 20:34:36 +0200

    add comments and minor variables name changes

--- src/main/java/com/verros/CheckDigit.java
+++ src/main/java/com/verros/CheckDigit.java
@@ -5,82 +5,90 @@ import java.util.Map;
 
 public class CheckDigit {
 
-
+    /**
+     * This method get a container Number, get rid the last number which is the check digit, generate the check digit
+     * and validate if is correct.
+     * @param containerNumber e.g. CSQU3054383
+     * @return True if the check digit is the same unless false
+     */
     public boolean verify(String containerNumber ){
 
         double sum = 0;
+        // Get the last number of the container number which is the check digit.
         int checkDigitToVerify = Integer.parseInt( containerNumber.substring(containerNumber.length() - 1) );
+
+        // Get rid of the last number, the last number is the check digit where we want to generated in order to validate.
         containerNumber = containerNumber.substring( 0, containerNumber.length() - 1 );
 
-        Map<Integer, Integer> checkDigitlastValue = new HashMap();
-        checkDigitlastValue.put(0, 0);
-        checkDigitlastValue.put(1, 1);
-        checkDigitlastValue.put(2, 2);
-        checkDigitlastValue.put(3, 3);
-        checkDigitlastValue.put(4, 4);
-        checkDigitlastValue.put(5, 5);
-        checkDigitlastValue.put(6, 6);
-        checkDigitlastValue.put(7, 7);
-        checkDigitlastValue.put(8, 8);
-        checkDigitlastValue.put(9, 9);
-        checkDigitlastValue.put(10, 0);
+        Map<Integer, Integer> checkDigitLastValue = new HashMap();
+        checkDigitLastValue.put(0, 0);
+        checkDigitLastValue.put(1, 1);
+        checkDigitLastValue.put(2, 2);
+        checkDigitLastValue.put(3, 3);
+        checkDigitLastValue.put(4, 4);
+        checkDigitLastValue.put(5, 5);
+        checkDigitLastValue.put(6, 6);
+        checkDigitLastValue.put(7, 7);
+        checkDigitLastValue.put(8, 8);
+        checkDigitLastValue.put(9, 9);
+        checkDigitLastValue.put(10, 0);
 
-        Map<Character, Integer> cd = new HashMap();
-        cd.put('0', 0);
-        cd.put('1', 1);
-        cd.put('2', 2);
-        cd.put('3', 3);
-        cd.put('4', 4);
-        cd.put('5', 5);
-        cd.put('6', 6);
-        cd.put('7', 7);
-        cd.put('8', 8);
-        cd.put('9', 9);
-        cd.put('A', 10);
-        cd.put('B', 12);
-        cd.put('C', 13);
-        cd.put('D', 14);
-        cd.put('E', 15);
-        cd.put('F', 16);
-        cd.put('G', 17);
-        cd.put('H', 18);
-        cd.put('I', 19);
-        cd.put('J', 20);
-        cd.put('K', 21);
-        cd.put('L', 23);
-        cd.put('M', 24);
-        cd.put('N', 25);
-        cd.put('O', 26);
-        cd.put('P', 27);
-        cd.put('Q', 28);
-        cd.put('R', 29);
-        cd.put('S', 30);
-        cd.put('T', 31);
-        cd.put('U', 32);
-        cd.put('V', 34);
-        cd.put('W', 35);
-        cd.put('X', 36);
-        cd.put('Y', 37);
-        cd.put('Z', 38);
+        Map<Character, Integer> equivalentNumericalValues = new HashMap();
+        equivalentNumericalValues.put('0', 0);
+        equivalentNumericalValues.put('1', 1);
+        equivalentNumericalValues.put('2', 2);
+        equivalentNumericalValues.put('3', 3);
+        equivalentNumericalValues.put('4', 4);
+        equivalentNumericalValues.put('5', 5);
+        equivalentNumericalValues.put('6', 6);
+        equivalentNumericalValues.put('7', 7);
+        equivalentNumericalValues.put('8', 8);
+        equivalentNumericalValues.put('9', 9);
+        equivalentNumericalValues.put('A', 10);
+        equivalentNumericalValues.put('B', 12);
+        equivalentNumericalValues.put('C', 13);
+        equivalentNumericalValues.put('D', 14);
+        equivalentNumericalValues.put('E', 15);
+        equivalentNumericalValues.put('F', 16);
+        equivalentNumericalValues.put('G', 17);
+        equivalentNumericalValues.put('H', 18);
+        equivalentNumericalValues.put('I', 19);
+        equivalentNumericalValues.put('J', 20);
+        equivalentNumericalValues.put('K', 21);
+        equivalentNumericalValues.put('L', 23);
+        equivalentNumericalValues.put('M', 24);
+        equivalentNumericalValues.put('N', 25);
+        equivalentNumericalValues.put('O', 26);
+        equivalentNumericalValues.put('P', 27);
+        equivalentNumericalValues.put('Q', 28);
+        equivalentNumericalValues.put('R', 29);
+        equivalentNumericalValues.put('S', 30);
+        equivalentNumericalValues.put('T', 31);
+        equivalentNumericalValues.put('U', 32);
+        equivalentNumericalValues.put('V', 34);
+        equivalentNumericalValues.put('W', 35);
+        equivalentNumericalValues.put('X', 36);
+        equivalentNumericalValues.put('Y', 37);
+        equivalentNumericalValues.put('Z', 38);
 
+        // Step 1,2 and 3
         for(int i = 0; i < containerNumber.length(); i++){
-            //Step 1,2 and 3
-            sum += cd.get( containerNumber.charAt(i)) * Math.pow( 2 , i);
+            sum += equivalentNumericalValues.get( containerNumber.charAt(i)) * Math.pow( 2 , i);
         }
 
-        //step 4
+        // Step 4
         int sumInteger = (int)(sum);
 
-        //step 5
+        // Step 5
         sumInteger /=  11;
 
-        //step 6
+        // Step 6
         sumInteger *=  11;
 
-        //step 6
+        // Step 6
         int last = (int) sum - sumInteger;
 
-        if( checkDigitToVerify == checkDigitlastValue.get(last % 10)){
+        if( checkDigitToVerify == checkDigitLastValue.get(last % 10)){
             return true;
         }else{
             return false;
--- src/main/java/com/verros/Main.java
+++ src/main/java/com/verros/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         CheckDigit checkDigit = new CheckDigit();
-
         System.out.println( checkDigit.verify("CSQU3054383") );
     }
 }
